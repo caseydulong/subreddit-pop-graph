216fd49d3654121cff7572a7579315b47e225c29
Casey DuLong <caseydulong@gmail.com>
2022-09-15 13:07:16 -0400

    Blocks out rough outline for adding subreddits to your search.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    // Required step: always call the parent class' constructor.
+    super(props)
+
+    // Set the state directly. Use props if necessary.
+    this.state = {
+      subreddits: []
+    }
+
+    // Note: think carefully before initializing.
+    // State based on props!
+    // someInitialValue: this.props.initialValue
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Subreddit Pop Graph</h1>
+          <p>
+            Enter subreddits to compare user populations.
+          </p>
+        </header>
+        <main>
+          <AddSubs/>
+        </main>
+      </div>
+    )
+  }
+}
+
+class AddSubs extends React.Component {
+  newSub() {
+
+  }
+
+  render() {
+    return (
+      <div className="Add-subs">
+        <AddSub
+          handleChange={this.handleChange}
+          deleteSub={this.deleteSub}
+        />
+        <AddSub
+          handleChange={this.handleChange}
+          deleteSub={this.deleteSub}
+        />
+        <button onClick={this.newSub}>+</button>
+      </div>
+    )
+  }
+}
+
+class AddSub extends React.Component {
+  removeSub() {
+    // TODO
+  }
+
+  render() {
+    return (
+      <div className="Add-sub">
+        <form>
+          <input
+            type="text"
+            onChange={this.props.handleChange} />
+        </form>
+        <button onClick={this.removeSub}>Delete</button>
+      </div>
+    )
+  }
+}
+
+class Graph extends React.Component {
+  // TODO
 }
 
 export default App;
