e92153a8c9d4cfb80ec41b6395f6b0f5dc7a0ec5
Casey DuLong <caseydulong@gmail.com>
2022-09-16 19:03:00 -0400

    User can now add and remove forms to enter subreddits. State is updated to contain a list of all subreddits.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,39 @@ import React from 'react';
 
 class App extends React.Component {
   constructor(props) {
-    // Required step: always call the parent class' constructor.
     super(props)
 
-    // Set the state directly. Use props if necessary.
     this.state = {
       subreddits: []
     }
 
-    // Note: think carefully before initializing.
-    // State based on props!
-    // someInitialValue: this.props.initialValue
+    this.newSubreddit = this.newSubreddit.bind(this)
+    this.removeSubreddit = this.removeSubreddit.bind(this)
+    this.editSubreddit = this.editSubreddit.bind(this)
   }
+
+  componentDidMount() {
+    this.setState({subreddits: [""]})
+  }
+
+  newSubreddit() {
+    let subreddits = this.state.subreddits
+    subreddits.push("")
+    this.setState({subreddits: subreddits})
+  }
+
+  removeSubreddit(event) {
+    let subreddits = this.state.subreddits
+    subreddits.splice(event.target.id, 1)
+    this.setState({subreddits: subreddits})
+  }
+
+  editSubreddit(event) {
+    let subreddits = this.state.subreddits
+    subreddits[event.target.id] = event.target.value
+    this.setState({subreddits: subreddits})
+  }
+
   render() {
     return (
       <div className="App">
@@ -25,49 +46,68 @@ class App extends React.Component {
           </p>
         </header>
         <main>
-          <AddSubs/>
+          <AddSubreddits
+            subreddits={this.state.subreddits}
+            newSubreddit={this.newSubreddit}
+            removeSubreddit={this.removeSubreddit}
+            editSubreddit={this.editSubreddit}
+          />
         </main>
       </div>
     )
   }
 }
 
-class AddSubs extends React.Component {
-  newSub() {
-
+class AddSubreddits extends React.Component {
+  renderSubreddit(i) {
+    return (
+      <Subreddit
+        id={i}
+        key={i}
+        value={this.props.subreddits[i]}
+        handleChange={this.props.editSubreddit}
+        removeSubreddit={this.props.removeSubreddit}
+      />
+    )
   }
 
   render() {
+    // Prepares array of Subreddit components for each subredit in state
+    let subreddits = []
+    for (let i = 0; i < this.props.subreddits.length; i++) {
+      subreddits.push(this.renderSubreddit(i))
+    }
+
     return (
-      <div className="Add-subs">
-        <AddSub
-          handleChange={this.handleChange}
-          deleteSub={this.deleteSub}
-        />
-        <AddSub
-          handleChange={this.handleChange}
-          deleteSub={this.deleteSub}
-        />
-        <button onClick={this.newSub}>+</button>
+      <div className="Add-subreddits">
+        {subreddits}
+        <button
+          type="button"
+          onClick={this.props.newSubreddit} >
+          +
+        </button>
       </div>
     )
   }
 }
 
-class AddSub extends React.Component {
-  removeSub() {
-    // TODO
-  }
-
+class Subreddit extends React.Component {
   render() {
     return (
-      <div className="Add-sub">
+      <div className="Subreddit">
         <form>
           <input
             type="text"
+            id={this.props.id}
+            value={this.props.value}
             onChange={this.props.handleChange} />
         </form>
-        <button onClick={this.removeSub}>Delete</button>
+        <button
+          type="button"
+          id={this.props.id}
+          onClick={this.props.removeSubreddit} >
+          Delete
+        </button>
       </div>
     )
   }
