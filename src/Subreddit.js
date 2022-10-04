4eecfc88e023e459201c0060f92815694e1042ba
Casey DuLong <caseydulong@gmail.com>
2022-10-04 14:46:08 -0400

    Validation and theme first pass - Adds some validation to subreddit fields - No API call if field is blank - Invalid message if subreddit is invalid; will not graph - Adds Material UI dark theme

--- src/Subreddit.js
+++ src/Subreddit.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { TextField, IconButton } from '@mui/material'
-import DeleteIcon from '@mui/icons-material/Delete'
+import { TextField, IconButton, Box } from '@mui/material'
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined'
 
 export default class Subreddit extends React.Component {
   render() {
     return (
-      <div className='Subreddit'>
+      <Box>
         <TextField
-          error={this.props.value === ''}
-          id={this.props.id}
           label='Enter Subreddit'
-          variant='outlined'
+          variant='filled'
+          id={this.props.id}
+          error={this.props.validation < 0}
+          helperText={this.props.helperText}
           value={this.props.value}
           onChange={this.props.handleChange}
           onBlur={this.props.searchSubreddit}
@@ -19,9 +20,9 @@ export default class Subreddit extends React.Component {
           id={this.props.id}
           onClick={this.props.removeSubreddit}
         >
-          <DeleteIcon />
+          <DeleteOutlineOutlinedIcon />
         </IconButton>
-      </div>
+      </Box>
     )
   }
 }
