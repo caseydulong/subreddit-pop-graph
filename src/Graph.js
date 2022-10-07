6cb8422ffac406cdf7bc1015283f3648acd13515
Casey DuLong <caseydulong@gmail.com>
2022-10-07 01:21:51 -0400

    Adds Graph-container wrapper div and class

--- src/Graph.js
+++ src/Graph.js
@@ -5,11 +5,13 @@ export default class Graph extends React.Component {
   render() {
     return (
       <div className='Graph'>
-        <Chart
-          options={this.props.options}
-          series={this.props.series}
-          type='bar'
-        />
+        <div className='Graph-container'>
+          <Chart
+            options={this.props.options}
+            series={this.props.series}
+            type='bar'
+          />
+        </div>
       </div>
     )
   }
